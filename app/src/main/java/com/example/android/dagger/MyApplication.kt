f8b7c1d122a2a5bd7cf88aa7485f3c7abcf389cd
mjkou <mjkou@thoughtworks.com>
2019-11-05 00:01:21 +0800

    fix android test by mock class

--- app/src/main/java/com/example/android/dagger/MyApplication.kt
+++ app/src/main/java/com/example/android/dagger/MyApplication.kt
@@ -19,12 +19,14 @@ package com.example.android.dagger
 import android.app.Application
 import com.example.android.dagger.di.AppComponent
 import com.example.android.dagger.di.DaggerAppComponent
-import com.example.android.dagger.storage.SharedPreferencesStorage
-import com.example.android.dagger.user.UserManager
 
 open class MyApplication : Application() {
 
     val appComponent: AppComponent by lazy {
-        DaggerAppComponent.factory().create(applicationContext)
+        initializeComponent()
+    }
+
+    open fun initializeComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
     }
 }
